fce1808a0a2bc6244ccc56f1899b734171bc3bd6
fujinoshinyu <sincan.1911@gmail.com>
2024-01-09 16:31:19 +0900

    commit2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<title>HOME</title>
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      </head>
+      <body>
+          <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('HOME') }}
+        </h2>
+    </x-slot>
+          <h1>HOME</h1>
+          
+          </x-app-layout>
+      </body>
+  </html>
